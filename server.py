9f11bcb2af48e2c10af6a31966bfc7bbffee99e6
Tristan Orlofski <tristanorlofski@gmail.com>
2018-04-13 22:42:54 +0000

    Deleted useless comments, modified index and winners to transpose table

--- server.py
+++ server.py
@@ -23,36 +23,13 @@ from flask_table import Table, Col
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
 # XXX: The URI should be in the format of: 
 #
 #     postgresql://USER:PASSWORD@35.227.79.146/proj1part2
-#
-# For example, if you had username gravano and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://gravano:foobar@35.227.79.146/proj1part2"
-#
 DATABASEURI = "postgresql://kdf2118:3632@35.227.79.146/proj1part2"
 
-
-#
 # This line creates a database engine that knows how to connect to the URI above.
-#
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-
 
 @app.before_request
 def before_request():
@@ -72,41 +49,14 @@ def before_request():
 
 @app.teardown_request
 def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't, the database could run out of memory!
-  """
   try:
     g.conn.close()
   except Exception as e:
     pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
   cursor2 = g.conn.execute("SELECT * FROM piece")
   dates2 = []
   for result in cursor2:
@@ -119,49 +69,8 @@ def index():
   for result in cursor:
     schools.append(result['school'])
   cursor.close()
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
- #context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
   return render_template("index.html", dates2=dates2, schools=schools)
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
 @app.route('/another')
 def another():
   return render_template("another.html")
@@ -210,11 +119,11 @@ def teammembers():
 
 @app.route('/winners', methods = ['POST'])
 def winners():
-  date = request.form['winners']
-  if winners == "all":
+  winners = request.form['winners']
+  if winners == "allw":
     cursor= g.conn.execute("SELECT * FROM winners ORDER BY piece_id")
   else:
-    cursor = g.conn.execute("SELECT * FROM winners WHERE date = '{0}' ORDER BY piece_id".format(date))
+    cursor = g.conn.execute("SELECT * FROM winners WHERE date = '{0}' ORDER BY piece_id".format(winners))
   pids = []
   lengths = []
   reps = []
@@ -223,6 +132,7 @@ def winners():
   dates = []
   rowers = []
   grads = []
+  isrecruit = []
   for result in cursor:
     pids.append(result['piece_id'])
     lengths.append(result['length'])
@@ -232,8 +142,9 @@ def winners():
     dates.append(result['date'])
     rowers.append(result['row_name'])
     grads.append(result['year'])
+    isrecruit.append(result['is_recruit'])
   cursor.close()
-  context = dict(data = (pids, lengths, reps, rest, splits, dates, rowers, grads))
+  context = dict(data = (pids, lengths, reps, rest, splits, dates, rowers, grads, isrecruit))
   return render_template("winners.html", **context)
 
 # Example of adding new data to the database
@@ -259,18 +170,6 @@ if __name__ == "__main__":
   @click.argument('HOST', default='0.0.0.0')
   @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using:
-
-        python server.py
-
-    Show the help text using:
-
-        python server.py --help
-
-    """
-
     HOST, PORT = host, port
     print "running on %s:%d" % (HOST, PORT)
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
